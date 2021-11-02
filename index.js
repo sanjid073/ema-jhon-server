bee32c49fb93a67cd7f83ccef9700de5d969e7ca
sanjid073 <sanjidmahe@gmail.com>
2021-11-03 01:14:03 +0600

    sdjsj

--- index.js
+++ index.js
@@ -72,6 +72,7 @@ async function run() {
         const cursor = orderCollection.find(query);
         const orders = await cursor.toArray();
         res.json(orders);
+        console.log("order hitted");
 
     })
       
